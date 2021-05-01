07b1c6adcf25a3622536e665157d9299474d1095
Camilo <edward.molano@uptc.edu.co>
2021-05-01 16:20:47 -0500

    Comentado

--- src/runner/Runner.java
+++ src/runner/Runner.java
@@ -7,7 +7,7 @@ public class Runner {
 
     //Permite iniciar el programa a partir de la instancia de una nueva instancia de Control
     public static void main(String[] args) {
-        //new Control();
+        new Control();
     }
 }
 
